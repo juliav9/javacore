1aed07ab7b286b21da9085cf9252dc61790a2c05
juliav9 <juliav9@gmail.com>
2016-10-10 21:23:19 +0300

    Lesson 3 Runner

---
+++ src/main/java/com/juliazubets/utils/convertors/PrimitiveConvertor.java
@@ -0,0 +1,23 @@
+package com.juliazubets.utils.convertors;
+
+public class PrimitiveConvertor {
+    /*
+    public char floatToChar(float float_variable_name){
+        char char_variable_name = float_variable_name;
+        System.out.println("Input float value is " + float_variable_name + ". Output char value is " + char_variable_name);
+        return char_variable_name;
+    }
+
+    public char intToChar (int int_variable_name){
+        char char_variable_name = int_variable_name;
+        System.out.println("Input int value is " + int_variable_name + ". Output char value is " + char_variable_name);
+        return char_variable_name;
+    }
+    */
+
+    public int charToInt (char char_variable_name){
+        int int_variable_name = char_variable_name;
+        System.out.println("Input char value is " + char_variable_name + ". Output int value is " + int_variable_name);
+        return int_variable_name;
+    }
+}
---
+++ src/main/java/com/juliazubets/utils/runners/Lesson3Runner.java
@@ -0,0 +1,14 @@
+package com.juliazubets.utils.runners;
+
+import com.juliazubets.utils.convertors.PrimitiveConvertor;
+
+/**
+ * Created by julia on 10/10/2016.
+ */
+public class Lesson3Runner {
+    public static void main (String args[]){
+        PrimitiveConvertor convertor = new PrimitiveConvertor();
+        char Variable = 'w';
+        convertor.charToInt(Variable);
+    }
+}
